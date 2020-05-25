570d4b823b34aa9ebfcf89be6864add6eefef7f4
Petr-Senfeld <59889161+Petr-Senfeld@users.noreply.github.com>
2020-05-25 16:06:32 +0200

    Update Regex_FindAndRemove.ps1

--- Find&Remove/Regex_FindAndRemove.ps1
+++ Find&Remove/Regex_FindAndRemove.ps1
@@ -1,19 +1,80 @@
-$TxtFile = Get-Content -Path ".\text.txt"
+##
+## promenne zacatek
+##
 
-#regular expresion & counter of lines
-$regExp = "\.google.com|\.google.cz"
+$txtFilePath = ".\hosts.txt"
+$newTxtFilePath = ".\text_new.txt"
+$approvedWebSites = Import-Csv -Delimiter ";" -Path ".\ApprovedWebSites.csv" 
+$additionalWebSites = Import-Csv -Delimiter ";" -Path ".\AdditionalBlockedWebSites.csv" 
+$switch = 1 # hodnota 0 (spusti pouze $approvedWebSites) nebo hodnota 1 (spusti approvedWebSites a additionalWebSites soubezne)
+
+##
+## promenne konec
+##
+
+$txtFile = Get-Content -Path $txtFilePath
 $linesCounter = 0
+$linesEdited = 0
+$date = Get-Date -format "HH:mm:ss"
+$skipper = 1
 
-foreach($line in $TxtFile)
+foreach($line in $txtFile)
 {
-    $linesCounter++
-    if($line -notmatch $regExp)
+    # spousti se $additionalWebSites & $approvedWebSites  pokud $switcher = 1
+    if($switch -eq 1)
     {
-    Add-Content -path ".\text_new.txt" -Value $line
-    $linesCounter--
+        # cyklus, ktery projde sloupec Domain v nactenem CSV souboru a porovnava ho jako regEx s každým řádkem
+        foreach($item in $approvedWebSites) 
+        {
+            if($line -match $item.Domain)
+            {
+                $lineTwo = ("`# " + $item.Comment + " `# " + "Schvalil: " + $item.ApprovedBy + " `# " + "Datum: " + $date +" `# "+ $line)
+                Add-Content -path $newTxtFilePath -Value $lineTwo
+                $linesCounter++
+                $linesEdited++
+                $skipper = 2
+            }        
+        }
+        # cyklus, ktery projde sloupec Domain v nactenem CSV souboru a porovnava ho jako regEx s každým řádkem
+        foreach($additionalLine in $additionalWebSites)
+        {
+            if($line -match $additionalLine.Domain)
+            {
+                $lineThree = ($line + " `# " + $additionalLine.Comment + " `# " + "Schvalil: " + $additionalLine.ApprovedBy + " `# " + "Datum: " + $date +" `# ")
+                Add-Content -path $newTxtFilePath -Value $lineThree
+                $linesCounter++
+                $linesEdited++
+                $skipper = 2
+            } 
+        }
+    }
+    # spousti se pouze $approvedWebSites pokud $switcher = 0
+    else
+    {
+        # cyklus, ktery projde sloupec Domain v nactenem CSV souboru a porovnava ho jako regEx s každým řádkem
+        foreach($item in $approvedWebSites) 
+        {
+            if($line -match $item.Domain)
+            {
+                $lineTwo = ("`# " + $item.Comment + " `# " + "Schvalil: " + $item.ApprovedBy + " `# " + "Datum: " + $date +" `# "+ $line)
+                Add-Content -path $newTxtFilePath -Value $lineTwo
+                $linesCounter++
+                $linesEdited++
+                $skipper = 2
+            }        
+        }
+    }
+    # pokud byl jiz radek upraven $skipper ho preskoci
+    if($skipper -eq 1)
+    {
+        Add-Content -path $newTxtFilePath -Value $line
+        $linesCounter++
+    }
+    elseif($skipper -eq 2)
+    {
+        $skipper = 1
     }
 }
-Write-Host "Bylo nalezeno: $linesCounter výrazů"
 
-#comparison used files
-compare-object (get-content .\text.txt) (get-content .\text_new.txt)
+Write-Host "Celkem přečteno: $linesCounter řádků"
+Write-Host "Celkem upraveno: $linesEdited řádků"
